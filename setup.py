634c5291467f6f052431ee8d139337d1c536b0df
xando <sebastian.pawlus@gmail.com>
2014-12-30 13:56:57 +0100

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='vmprof',
     author='vmprof team',
     author_email='sebastian.pawlus@gmail.com',
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     description="Python's vmprof client",
     install_requires=[
