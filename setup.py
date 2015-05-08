75c74dfc219c0d205a4a6d5642e3c64009273f3f
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-05-08 17:19:26 +0200

    version bump

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     name='vmprof',
     author='vmprof team',
     author_email='sebastian.pawlus@gmail.com',
-    version="0.0.8",
+    version="0.0.9",
     packages=find_packages(),
     description="Python's vmprof client",
     install_requires=[
