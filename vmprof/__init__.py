81b7b4360a5c012fd9f1fd477915da24c7bfec9c
Antonio Cuni <anto.cuni@gmail.com>
2015-08-07 19:35:46 +0200

    make it possible to upload an already saved file by using python -m vmprof.com

--- vmprof/__init__.py
+++ vmprof/__init__.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-from . import com
 from . import cli
 
 import _vmprof
--- vmprof/com.py
+++ vmprof/com.py
@@ -1,27 +1,26 @@
+import sys
 import json
 import six.moves.urllib.request as request
+import click
 
+import vmprof
 
-def send(t, args):
-
-    name = args.program
-    base_url = args.web_url
-    auth = args.web_auth
+def upload(stats, name, argv, host, auth):
 
     data = {
-        "VM": t.interp,
-        "profiles": t.get_tree()._serialize(),
-        "argv": "%s %s" % (name, " ".join(args.args)),
+        "VM": stats.interp,
+        "profiles": stats.get_tree()._serialize(),
+        "argv": "%s %s" % (name, argv),
         "version": 1,
     }
 
     data = json.dumps(data).encode('utf-8')
 
     # XXX http only for now
-    if base_url.startswith("http"):
-        url = '%s/api/log/' % base_url.rstrip("/")
+    if host.startswith("http"):
+        url = '%s/api/log/' % host.rstrip("/")
     else:
-        url = 'http://%s/api/log/' % base_url.rstrip("/")
+        url = 'http://%s/api/log/' % host.rstrip("/")
 
     headers = {'content-type': 'application/json'}
 
@@ -32,4 +31,21 @@ def send(t, args):
 
     res = request.urlopen(req)
     val = res.read()
-    return "http://" + base_url + "/#/" + val[1:-1]
+    return "http://" + host + "/#/" + val[1:-1]
+
+
+
+@click.command()
+@click.argument('profile', type=str)
+@click.option('--web-url', type=str, default='vmprof.baroquesoftware.com',
+              help='Target host')
+@click.option('--web-auth', type=str, default=None,
+              help='Authtoken for your acount on the server')
+def main(profile, web_url, web_auth): 
+    stats = vmprof.read_profile(profile, virtual_only=True)
+    sys.stderr.write("Compiling and uploading to %s...\n" % (web_url,))
+    res = upload(stats, profile, [], web_url, web_auth)
+    sys.stderr.write("Available at:\n%s\n" % res)
+
+if __name__ == '__main__':
+    main()
