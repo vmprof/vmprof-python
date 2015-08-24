54a473ac2a89e82e467ec2806924fb3b9708eede
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-08-24 13:52:28 +0200

    testing travis

--- tests/test_travis.py
+++ tests/test_travis.py
@@ -1,10 +1,19 @@
+import time
 import vmprof
 import tempfile
+
+
+def function_foo():
+    t0 = time.time()
+    t = time.time() - t0 < 0.5
+    while t:
+        t = time.time() - t0 < 0.5
+        print time.time() - t0
 
 
 def test_travis_1():
     tmpfile = tempfile.NamedTemporaryFile(dir=".")
     vmprof.enable(tmpfile.fileno())
-    # function_foo()
+    function_foo()
     vmprof.disable()
-    assert b"function_foo" in open(tmpfile.name, 'rb').read()
+    assert tmpfile.name
