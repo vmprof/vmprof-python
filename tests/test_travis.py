fc2b236b5044a1a19e89332a4266662d0e2bd9a2
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-08-24 13:43:19 +0200

    testing travis

--- tests/test_travis.py
+++ tests/test_travis.py
@@ -1,6 +1,10 @@
+import vmprof
 import tempfile
 
 
 def test_travis_1():
     tmpfile = tempfile.NamedTemporaryFile(dir=".")
-    assert tmpfile.name
+    vmprof.enable(tmpfile.fileno())
+    # function_foo()
+    vmprof.disable()
+    assert b"function_foo" in open(tmpfile.name, 'rb').read()
