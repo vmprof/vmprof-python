0ec12571fd4e59ab5e36da99cacd044125e5c171
Antonio Cuni <anto.cuni@gmail.com>
2015-09-17 02:43:22 +0200

    make vmprof.upload to use the new CallGraph-based format (to be used in conjunction with a vmprof-server on the newflames branch); it is still possible to upload in the old format by using --v1

--- vmprof/upload.py
+++ vmprof/upload.py
@@ -49,6 +49,14 @@ def upload_json(data, host, auth):
     val = res.read()
     return "http://" + host + "/#/" + val[1:-1]
 
+def read_and_upload_v2(filename, web_url, web_auth):
+    from vmprof.callgraph import CallGraph
+    profiles, interp_name, addrspace = vmprof.read_profile(filename, virtual_only=False,
+                                                           include_extra_info=False,
+                                                           load_jit_symbols=True)
+    callgraph = CallGraph.from_profiles(addrspace, profiles)
+    res = upload_v2(interp_name, callgraph, filename, [], web_url, web_auth)
+    return res
 
 
 @click.command()
@@ -57,10 +65,21 @@ def upload_json(data, host, auth):
               help='Target host')
 @click.option('--web-auth', type=str, default=None,
               help='Authtoken for your acount on the server')
-def main(profile, web_url, web_auth): 
-    stats = vmprof.read_stats(profile, virtual_only=True)
+@click.option('--local', '-l', is_flag=True,
+              help='Same as --web-url localhost:8000')
+@click.option('--v1', is_flag=True,
+              help='Use version 1 of the upload format, to be used for old servers')
+def main(profile, web_url, web_auth, local, v1):
+    if local:
+        web_url = 'localhost:8000'
+    #
     sys.stderr.write("Compiling and uploading to %s...\n" % (web_url,))
-    res = upload_v1(stats, profile, [], web_url, web_auth)
+    if v1:
+        stats = vmprof.read_stats(profile, virtual_only=True)
+        res = upload_v1(stats, profile, [], web_url, web_auth)
+    else:
+        res = read_and_upload_v2(profile, web_url, web_auth)
+    #
     sys.stderr.write("Available at:\n%s\n" % res)
 
 if __name__ == '__main__':
