b71ed92c0bf788cde2ab4d9b54e9134d9ee260fa
Antonio Cuni <anto.cuni@gmail.com>
2015-09-25 01:09:46 +0200

    fix vmprof.__main__

--- vmprof/__main__.py
+++ vmprof/__main__.py
@@ -32,7 +32,7 @@ def upload_stats(stats, args):
     auth = args.web_auth
     #
     sys.stderr.write("Compiling and uploading to %s...\n" % (args.web_url,))
-    res = vmprof.upload.upload(stats, name, argv, host, auth)
+    res = vmprof.upload.upload_v1(stats, name, argv, host, auth)
     sys.stderr.write("Available at:\n%s\n" % res)
     
 
