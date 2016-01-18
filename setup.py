0a1a6d11d47ab270c85ee4aade35cc56ef64aa83
Maciej Fijalkowski <fijall@gmail.com>
2016-01-18 10:48:05 +0100

    bump a minor version info

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name='vmprof',
     author='vmprof team',
     author_email='fijal@baroquesoftware.com',
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     description="Python's vmprof client",
     install_requires=[
