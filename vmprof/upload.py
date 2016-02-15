eed1c511b44f3c14136e86954c518bb5992b9c82
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-02-15 20:35:57 +0100

    click removed from upload

--- vmprof/upload.py
+++ vmprof/upload.py
@@ -1,10 +1,12 @@
 import sys
 import json
+import argparse
 import six.moves.urllib.request as request
-import click
+
 
 import vmprof
 PY3 = sys.version_info[0] >= 3
+
 
 def upload(stats, name, argv, host, auth):
 
@@ -39,17 +41,19 @@ def upload(stats, name, argv, host, auth):
     return "%s/#/%s" % (host, val)
 
 
-@click.command()
-@click.argument('profile', type=str)
-@click.option('--web-url', type=str, default='http://vmprof.com',
-              help='Target host')
-@click.option('--web-auth', type=str, default=None,
-              help='Authtoken for your acount on the server')
-def main(profile, web_url, web_auth): 
-    stats = vmprof.read_profile(profile, virtual_only=True)
-    sys.stderr.write("Compiling and uploading to %s...\n" % (web_url,))
-    res = upload(stats, profile, [], web_url, web_auth)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("profile")
+    parser.add_argument("--web-url", default='http://vmprof.com', help='Target host')
+    parser.add_argument("--web-auth", default=None, help='Authtoken for your acount on the server')
+    args = parser.parse_args()
+
+    stats = vmprof.read_profile(args.profile, virtual_only=True)
+    sys.stderr.write("Compiling and uploading to %s...\n" % args.web_url)
+
+    res = upload(stats, args.profile, [], args.web_url, args.web_auth)
     sys.stderr.write("Available at:\n%s\n" % res)
+
 
 if __name__ == '__main__':
     main()
