8b2ef154c0da32398296740e9c94942e076298a9
Richard Plangger <planrichi@gmail.com>
2016-07-14 11:53:17 -0500

    add .devN suffix to version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     name='vmprof',
     author='vmprof team',
     author_email='fijal@baroquesoftware.com',
-    version="0.3.0",
+    version="0.3.0.dev0",
     packages=find_packages(),
     description="Python's vmprof client",
     long_description='See https://vmprof.readthedocs.org/',
