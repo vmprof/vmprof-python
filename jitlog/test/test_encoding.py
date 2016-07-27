f901541bcc02175ea5850b522f0ccbada86314d2
Richard Plangger <planrichi@gmail.com>
2016-07-27 10:26:31 +0200

    two more tests to ensure the encoding such as utf-8 and latin-1 can be read

--- jitlog/test/test_encoding.py
+++ jitlog/test/test_encoding.py
@@ -6,22 +6,27 @@ from jitlog import constants as const
 
 PY3 = sys.version_info[0] >= 3
 
-@py.test.mark.parametrize('encoding,text,decoded',
-    [('ascii', b'a!1%$', u'a!1%$')])
-def test_merge_point_extract_source_code(encoding,text,decoded,tmpdir):
+@py.test.mark.parametrize('encoding,text,decoded,bom',
+    [('ascii',   b'a!1%$', u'a!1%$', None),
+     ('utf-8',   b"\x41\xE2\x89\xA2\xCE\x91\x2E", u'A≢Α.', None),
+     ('latin-1', b'\xDCber', u'Über', None),
+    ])
+def test_merge_point_extract_source_code(encoding,text,decoded,bom,tmpdir):
     forest = TraceForest(1)
     trace = forest.add_trace('loop', 0, 0)
     trace.start_mark(const.MARK_TRACE_OPT)
     file = tmpdir.join("file"+encoding+".py")
     l = []
-    l.append(b"# coding: ")
+    if bom:
+        l.append(bom)
+    l.append("# coding: ".encode(encoding))
     l.append(encoding.encode(encoding))
-    l.append(b"\r\n")
-    l.append(b"print(\"")
+    l.append("\r\n".encode(encoding))
+    l.append("print(\"".encode(encoding))
     l.append(text)
-    l.append(b"\")")
-    l.append(b"\r\n")
-    file.write(b''.join(l))
+    l.append("\")".encode(encoding))
+    l.append("\r\n".encode(encoding))
+    file.write_binary(b''.join(l))
     trace.add_instr(MergePoint({0x1: str(file), 0x2: 2}))
     forest.extract_source_code_lines()
     assert forest.source_lines[str(file)][2] == (0, "print(\"" + decoded + "\")")
