172e8bda1f427ef5b8a47631eb9208b13230080f
Richard Plangger <planrichi@gmail.com>
2016-09-29 12:10:27 +0200

    remove pytest.skip for windows

--- jitlog/test/__init__.py
+++
@@ -1,7 +0,0 @@
-import py
-import platform
-
-if platform.system() == 'Windows':
-    """ this is a low hanging fruit! there should be little work
-        to get this going """
-    py.test.skip("jitlog is not supported on windows (yet)")
