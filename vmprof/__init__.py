2ec8813b7d890df27bc770717012e1366f69d1a4
Richard Plangger <planrichi@gmail.com>
2016-11-07 17:16:18 +0100

    disable the gzipped cpu profiles for now

--- vmprof/__init__.py
+++ vmprof/__init__.py
@@ -59,6 +59,13 @@ def _gzip_start(fileno):
 
     Return the subprocess' input fileno.
     """
+    # XXX During the sprint in munich we found several issues
+    # on bigger applications running vmprof. For instance:
+    # coala or some custom medium sized scripts.
+    #
+    # For now I will disable this feature. To enable it we should run
+    # bigger applications.
+    return fileno
     # Prefer system gzip and fall back to Python's gzip module
     if which("gzip"):
         gzip_cmd = ["gzip", "-", "-4"]
