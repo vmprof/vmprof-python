27d89de1b96dd4b7e8d4a51d2dcc350d1ae378e4
Richard Plangger <planrichi@gmail.com>
2016-11-24 14:41:25 +0100

    add wheel files for linux

---
+++ travis/build-wheels.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e -x
+
+# Install a system package required by our library
+# yum install -y atlas-devel
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    ${PYBIN}/pip install -r /io/dev-requirements.txt
+    ${PYBIN}/pip wheel /io/ -w wheels
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair $whl -w /io/wheels/
+done
+
+# Install packages and test
+# for PYBIN in /opt/python/*/bin/; do
+#     ${PYBIN}/pip install vmprof --no-index -f /io/wheels
+#     (cd $HOME; ${PYBIN}/nosetests pymanylinuxdemo)
+# done
