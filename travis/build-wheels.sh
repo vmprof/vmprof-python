99e378a09a5fe519a26b12f43fb8e217fcba7c61
Richard Plangger <planrichi@gmail.com>
2016-12-07 08:49:47 +0100

    adding upload script with secure upload procedure

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -15,7 +15,10 @@ done
 
 # Bundle external shared libraries into the wheels
 for whl in wheels/*.whl; do
-    auditwheel repair $whl -w /io/wheels/
+    if [[ $whl == *"vmprof"* ]]
+    then
+        auditwheel repair $whl -w /io/wheels/
+    fi
 done
 
 # Install packages and test
