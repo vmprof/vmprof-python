30e7360a9dcf1ce0f01f3769c429119ef2399ff7
Richard Plangger <planrichi@gmail.com>
2016-12-21 13:44:59 +0100

    use after success, deploy does not seem to work

--- travis/upload-artifact.sh
+++ travis/upload-artifact.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-pip install virtualenv
-
-virtualenv venv
-source venv/bin/activate
-
+echo "deploy stage after success. uploading files..."
 pip install twine
-
-
 VERSION="$(python setup.py --version)"
 
-if [ -n "$UPLOAD_SDIST" ]; then
-    echo "uploading source distribution"
+if [[ "$UPLOAD_SDIST" == "1" ]]; then
+    echo " -> uploading source distribution"
     python setup.py sdist
-    twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD vmprof-$(VERSION).tar.gz
+    twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/vmprof-$(VERSION).tar.gz
 fi
 
-if [ -n "$BUILD_LINUX_WHEEL" ]; then
-    echo "uploading built wheels"
-    twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD vmprof-$(VERSION)*.whl
+if [[ "$BUILD_LINUX_WHEEL" == "1" ]]; then
+    echo " -> uploading wheels"
+    twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD wheels/vmprof-$(VERSION)*.whl
 fi
 
