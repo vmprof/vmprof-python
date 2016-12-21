99c51f70608e3fef573915aa05a7770e7e7bf1aa
Richard Plangger <planrichi@gmail.com>
2016-12-21 13:11:19 +0100

    upload when tags are pushed

--- travis/upload-artifact.sh
+++ travis/upload-artifact.sh
@@ -7,9 +7,18 @@ source venv/bin/activate
 
 pip install twine
 
-python setup.py sdist
 
 VERSION="$(python setup.py --version)"
+PYVERSION="$(python --version)"
 
-twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD vmprof-$(VERSION).tar.gz
-twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD vmprof-$(VERSION)*.whl
+if [[ $PYVERSION == *"Python 2.7"* ]]; then
+    echo "uploading source distribution"
+    python setup.py sdist
+    twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD vmprof-$(VERSION).tar.gz
+fi
+
+if [ -n "$BUILD_LINUX_WHEEL" ]; then
+    echo "uploading built wheels"
+    twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD vmprof-$(VERSION)*.whl
+fi
+
