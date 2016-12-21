4cf32cc185de61fd13aa8acff8a600e224ed206d
Richard Plangger <planrichi@gmail.com>
2016-12-21 14:09:20 +0100

    use env variable not $(variable)

--- travis/upload-artifact.sh
+++ travis/upload-artifact.sh
@@ -7,11 +7,11 @@ VERSION="$(python setup.py --version)"
 if [[ "$UPLOAD_SDIST" == "1" ]]; then
     echo " -> uploading source distribution"
     python setup.py sdist
-    twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/vmprof-$(VERSION).tar.gz
+    twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/vmprof-$VERSION.tar.gz
 fi
 
 if [[ "$BUILD_LINUX_WHEEL" == "1" ]]; then
     echo " -> uploading wheels"
-    twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD wheels/vmprof-$(VERSION)*.whl
+    twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD wheels/vmprof-$VERSION*.whl
 fi
 
