9224cb505131b70700181cb15d416f8d0c0a4df7
Richard Plangger <planrichi@gmail.com>
2016-12-21 15:26:41 +0100

    build mac wheels and upload them on tag builds

--- travis/upload-artifact.sh
+++ travis/upload-artifact.sh
@@ -10,6 +10,18 @@ if [[ "$UPLOAD_SDIST" == "1" ]]; then
     twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/vmprof-$VERSION.tar.gz
 fi
 
+if [[ "$MAC_WHEEL" == "1" ]]; then
+    echo " -> uploading mac wheel distribution"
+    pip install auditwheel
+    pip wheel . -w dist
+    for whl in dist/*.whl; do
+        if [[ $whl == *"vmprof"* ]]; then
+            auditwheel repair $whl -w dist/
+        fi
+    done
+    twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/vmprof-$VERSION.tar.gz
+fi
+
 if [[ "$BUILD_LINUX_WHEEL" == "1" ]]; then
     echo " -> uploading wheels"
     twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD wheels/vmprof-$VERSION*.whl
