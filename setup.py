aae88e5c96a0019e351408c7bce61a17bb83e79e
Richard Plangger <planrichi@gmail.com>
2016-12-21 13:21:39 +0100

    version bump

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     name='vmprof',
     author='vmprof team',
     author_email='fijal@baroquesoftware.com',
-    version="0.4.0.dev1",
+    version="0.4.0.dev2",
     packages=find_packages(),
     description="Python's vmprof client",
     long_description='See https://vmprof.readthedocs.org/',
