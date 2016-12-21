8c47ed5259d967368ee819cd24f2ff386f537b12
Richard Plangger <planrichi@gmail.com>
2016-12-21 15:00:17 +0100

    speed up travis builds, only use pyenv for mac now

--- travis/install.sh
+++ travis/install.sh
@@ -2,16 +2,16 @@
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
+    git clone --depth 1 https://github.com/yyuu/pyenv.git ~/.pyenv
+    PYENV_ROOT="$HOME/.pyenv"
+    PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+    pyenv install $PYENV
+    pyenv global $PYENV
+    pyenv rehash
+    python -m pip install --user virtualenv
+
+    python -m virtualenv ~/.venv
+    source ~/.venv/bin/activate
 fi
 
-git clone --depth 1 https://github.com/yyuu/pyenv.git ~/.pyenv
-PYENV_ROOT="$HOME/.pyenv"
-PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-pyenv install $PYENV
-pyenv global $PYENV
-pyenv rehash
-python -m pip install --user virtualenv
-
-python -m virtualenv ~/.venv
-source ~/.venv/bin/activate
