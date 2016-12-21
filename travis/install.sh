82eec60a85a06b6a7b09d3721aa8271d39d8d3e7
Richard Plangger <planrichi@gmail.com>
2016-12-21 14:43:51 +0100

    add pyenv to get the right python version

---
+++ travis/install.sh
@@ -0,0 +1,17 @@
+
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    brew update || brew update
+fi
+
+git clone --depth 1 https://github.com/yyuu/pyenv.git ~/.pyenv
+PYENV_ROOT="$HOME/.pyenv"
+PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+pyenv install $PYENV
+pyenv global $PYENV
+pyenv rehash
+python -m pip install --user virtualenv
+
+python -m virtualenv ~/.venv
+source ~/.venv/bin/activate
