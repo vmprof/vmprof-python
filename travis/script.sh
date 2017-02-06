90c567e5f312f3f3c9c6fcdb0fbaace2634ce376
Richard Plangger <planrichi@gmail.com>
2017-02-06 10:39:42 +0100

    expand variable inside double quotes, otherwise this expression part is always true

--- travis/script.sh
+++ travis/script.sh
@@ -9,7 +9,7 @@ fi
 py.test vmprof/ -vrs
 py.test jitlog/ -vrs
 
-if [[ -n "TRAVIS_TAG" && "$BUILD_LINUX_WHEEL" == "1" ]]; then
+if [[ -n "$TRAVIS_TAG" && "$BUILD_LINUX_WHEEL" == "1" ]]; then
     docker pull quay.io/pypa/manylinux1_x86_64
     docker pull quay.io/pypa/manylinux1_i686
     docker run --rm -v `pwd`:/io:Z quay.io/pypa/manylinux1_x86_64 bash /io/travis/build-wheels.sh;
