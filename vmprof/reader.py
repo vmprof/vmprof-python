7737e24c502f81380ef10a59df3b4b2bb39621d1
Richard Plangger <planrichi@gmail.com>
2017-02-13 18:00:23 +0100

    remove debugging output, step once more on linux

--- vmprof/reader.py
+++ vmprof/reader.py
@@ -270,8 +270,6 @@ class LogReader(object):
             elif marker == MARKER_VIRTUAL_IP or marker == MARKER_NATIVE_SYMBOLS:
                 unique_id = self.read_addr()
                 name = self.read_string()
-                if marker == MARKER_NATIVE_SYMBOLS:
-                    print(hex(unique_id), name)
                 s.virtual_ips.append((unique_id, name))
             elif marker == MARKER_TRAILER:
                 #if not virtual_ips_only:
