e88a3845e7b0461dc3e195215f657031d8c4c1b2
Richard Plangger <planrichi@gmail.com>
2017-02-15 14:23:20 +0100

    oh well, mac wheels were not uploaded, do that now

--- travis/script.sh
+++ travis/script.sh
@@ -9,10 +9,19 @@ fi
 py.test vmprof/ -vrs
 py.test jitlog/ -vrs
 
-if [[ -n "$TRAVIS_TAG" && "$BUILD_LINUX_WHEEL" == "1" ]]; then
-    docker pull quay.io/pypa/manylinux1_x86_64
-    docker pull quay.io/pypa/manylinux1_i686
-    docker run --rm -v `pwd`:/io:Z quay.io/pypa/manylinux1_x86_64 bash /io/travis/build-wheels.sh;
-    docker run --rm -v `pwd`:/io:Z quay.io/pypa/manylinux1_i686 linux32 bash /io/travis/build-wheels.sh;
-    bash travis/upload-artifact.sh
+    if [[ "$MAC_WHEEL" == "1" ]]; then
+        bash travis/upload-artifact.sh
+    fi
+
+if [[ -n "$TRAVIS_TAG" ]]; then
+
+
+    if [[ "$BUILD_LINUX_WHEEL" == "1" ]]; then
+        docker pull quay.io/pypa/manylinux1_x86_64
+        docker pull quay.io/pypa/manylinux1_i686
+        docker run --rm -v `pwd`:/io:Z quay.io/pypa/manylinux1_x86_64 bash /io/travis/build-wheels.sh;
+        docker run --rm -v `pwd`:/io:Z quay.io/pypa/manylinux1_i686 linux32 bash /io/travis/build-wheels.sh;
+        bash travis/upload-artifact.sh
+    fi
+
 fi
