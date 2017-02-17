18a205d78c68bd16c4ef2dfc9dabaf3f109be299
Richard Plangger <planrichi@gmail.com>
2017-02-17 15:51:16 +0100

    do NOT use PyGILState_GetThisThreadState, it may deadlock. use other CPython API to fish the right thread state

--- src/vmprof.h
+++ src/vmprof.h
@@ -54,6 +54,7 @@ int IS_VMPROF_EVAL(void * ptr);
 // for cpython
 #include "_vmprof.h"
 #include <Python.h>
+#include <pythread.h>
 #include <frameobject.h>
 #define PY_STACK_FRAME_T PyFrameObject
 #define PY_EVAL_RETURN_T PyObject
