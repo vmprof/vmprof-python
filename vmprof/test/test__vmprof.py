d5cbe361de974b0351164aa9b6e0e91bc0a188a1
Antonio Cuni <anto.cuni@gmail.com>
2017-11-11 14:43:25 +0100

    try to follow richard's suggestion to call also _vmprof.stop_sampling before _vmprof.disable, let's see if this fixes tests on mac

--- vmprof/test/test__vmprof.py
+++ vmprof/test/test__vmprof.py
@@ -2,26 +2,32 @@ import pytest
 import sys
 import _vmprof
 
+def enable_and_disable(fname, *args, **kwargs):
+    with fname.open('w+b') as f:
+        try:
+            if not kwargs:
+                # try hard to pass kwargs == NULL, instead of {}
+                return _vmprof.enable(f.fileno(), *args)
+            else:
+                return _vmprof.enable(f.fileno(), *args, **kwargs)
+        finally:
+            _vmprof.stop_sampling()
+            _vmprof.disable()
+
 def test_enable(tmpdir):
     prof = tmpdir.join('a.vmprof')
-    with prof.open('w+b') as f:
-        ret = _vmprof.enable(f.fileno(), 0.004)
-        _vmprof.disable()
+    ret = enable_and_disable(prof, 0.004)
     assert ret is None
 
 def test_enable_options(tmpdir):
     prof = tmpdir.join('a.vmprof')
-    with prof.open('w+b') as f:
-        ret = _vmprof.enable(f.fileno(), 0.004, lines=True)
-        _vmprof.disable()
+    ret = enable_and_disable(prof, 0.004, lines=True)
     assert ret == {}
 
 def test_enable_options_unix_only(tmpdir):
     prof = tmpdir.join('a.vmprof')
-    with prof.open('w+b') as f:
-        ret = _vmprof.enable(f.fileno(), 0.004, memory=True, native=True,
+    ret = enable_and_disable(prof, 0.004, memory=True, native=True,
                              real_time=True)
-        _vmprof.disable()
     if sys.platform == 'win32':
         assert ret == {'memory': True, 'native': True, 'real_time': True}
     else:
@@ -29,7 +35,5 @@ def test_enable_options_unix_only(tmpdir):
 
 def test_enable_options_unknown(tmpdir):
     prof = tmpdir.join('a.vmprof')
-    with prof.open('w+b') as f:
-        ret = _vmprof.enable(f.fileno(), 0.004, foo=1, bar=2)
-        _vmprof.disable()
+    ret = enable_and_disable(prof, 0.004, foo=1, bar=2)
     assert ret == {'foo': 1, 'bar': 2}
