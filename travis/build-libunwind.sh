c3f1999e48f72609bc5dbdba41d0a673b3e64ab9
Antonio Cuni <anto.cuni@gmail.com>
2019-05-29 18:06:13 +0200

    we run build-libunwind.sh inside the manylinux image: wget is not present but curl is

--- travis/build-libunwind.sh
+++ travis/build-libunwind.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install -y libtool
 cd
-wget http://download.savannah.gnu.org/releases/libunwind/libunwind-1.2.tar.gz
+curl -O http://download.savannah.gnu.org/releases/libunwind/libunwind-1.2.tar.gz
 tar xf libunwind-1.2.tar.gz
 cd libunwind-1.2/
 ./configure
