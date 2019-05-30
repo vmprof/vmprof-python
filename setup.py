0f012e9e0a6c7022b91534c3156c47ac6541e034
Ronan Lamy <ronan.lamy@gmail.com>
2019-05-30 01:46:22 +0100

    Bump version again
    The '0.4.14' tag points to a broken commit, so we need a new version.

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ setup(
     name='vmprof',
     author='vmprof team',
     author_email='fijal@baroquesoftware.com',
-    version="0.4.14",
+    version="0.4.15",
     packages=find_packages(),
     description="Python's vmprof client",
     long_description='See https://vmprof.readthedocs.org/',
