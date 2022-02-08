8767914b6ff133342f1c69f0fe94dfd8d79ac4fa
mattip <matti.picus@gmail.com>
2022-02-08 09:44:16 +0200

    on aarch64 and ppc64le, build libunwind from sources

--- travis/build-libunwind.sh
+++ travis/build-libunwind.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-yum install -y libtool
+set -ex
+yum install -y elfutils-libelf-devel libdwarf-devel libtool
 cd
-curl -L -O http://download.savannah.gnu.org/releases/libunwind/libunwind-1.2.tar.gz
-tar xf libunwind-1.2.tar.gz
-cd libunwind-1.2/
+curl -L -O http://download.savannah.nongnu.org/releases/libunwind/libunwind-1.5.0.tar.gz
+tar xf libunwind-1.5.0.tar.gz
+cd libunwind-1.5.0/
+autoreconf -i
 ./configure
+make
 make install
