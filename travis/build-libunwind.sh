b988becc7300fef64923366ab56376664f09539f
mattip <matti.picus@gmail.com>
2024-09-27 12:21:41 +0300

    remove vestiges of unused travis builds

--- travis/build-libunwind.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum install -y libtool
-cd
-curl -L -O http://download.savannah.gnu.org/releases/libunwind/libunwind-1.2.tar.gz
-tar xf libunwind-1.2.tar.gz
-cd libunwind-1.2/
-./configure
-make install
